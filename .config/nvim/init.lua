eda3da55a1047c43701de04a36495ca2a9c9250e
agustinvalencia <valencia.ag@gmail.com>
2025-02-09 19:18:23 +0100

    sync from mini

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -696,6 +696,30 @@ require('lazy').setup({
             },
           },
           ts_ls = {},
+          {
+            texlab = {
+              bibtexFormatter = 'texlab',
+              build = {
+                args = { '-pdf', '-interaction=nonstopmode', '-synctex=1', '%f' },
+                executable = 'latexmk',
+                forwardSearchAfter = false,
+                onSave = false,
+              },
+              chktex = {
+                onEdit = false,
+                onOpenAndSave = false,
+              },
+              diagnosticsDelay = 300,
+              formatterLineLength = 80,
+              forwardSearch = {
+                args = {},
+              },
+              latexFormatter = 'latexindent',
+              latexindent = {
+                modifyLineBreaks = false,
+              },
+            },
+          },
         },
         -- Ensure the servers and tools above are installed
         --  To check the current status of installed tools and/or manually install
