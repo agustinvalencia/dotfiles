1c7380a0f007785265bf22df1c944d00f1c27997
agustinvalencia <valencia.ag@gmail.com>
2025-05-18 17:35:21 +0200

    update lsps

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,28 @@
 -- treesitter.lua
 return {
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function()
-        require("nvim-treesitter.configs").setup({
-          ensure_installed = { 
-                    "lua", 
-                    "tsx",
-                    "bash",
-                    "json",
-                    "yaml",
-                    "regex",
-                    "python", 
-                    "javascript", 
-                    "typescript", 
-                },
-          highlight        = { enable = true },
-          indent = {enable = true}
-        })
-      end,
-    },
-  }
+  { "fei6409/log-highlight.nvim", event = "BufRead *.log", opts = {} },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua",
+          "tsx",
+          "bash",
+          "json",
+          "yaml",
+          "regex",
+          "python",
+          "javascript",
+          "typescript",
+          "toml",
+          "css",
+          "rust",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+}
