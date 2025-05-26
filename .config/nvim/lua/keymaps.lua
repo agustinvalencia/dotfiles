369ad704661a00099209fa44024e779db4064b2c
agustin valencia <valencia.ag@gmail.com>
2025-05-26 10:06:50 +0200

    added shortcut for vertical-horizontal terminal splits

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,2 +1,17 @@
+-- editor buffer split
 vim.keymap.set("n", "<leader>u-", "<cmd>split<cr>", { desc = "Horizontal Split" })
 vim.keymap.set("n", "<leader>u\\", "<cmd>vsplit<cr>", { desc = "Vertical Split" })
+
+-- terminal split
+vim.keymap.set("n", "<leader>tv", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("L")
+end, { desc = "[T]erminal [V]ertical" })
+
+vim.keymap.set("n", "<leader>th", function()
+  vim.cmd.new()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 10)
+end, { desc = "[T]erminal [H]orizontal" })
